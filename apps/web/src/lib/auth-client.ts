7e56139858cdcc24e8503c7c92e0fcd9108aa7cb
João Heitor <jhmartinsrdo@gmail.com>
2026-02-04 20:24:21 -0300

    feat: login with google added

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -4,3 +4,10 @@ import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   baseURL: env.NEXT_PUBLIC_SERVER_URL,
 });
+
+export const loginSocial = async () => {
+  return authClient.signIn.social({
+    provider: "google",
+    callbackURL: `${env.NEXT_PUBLIC_APP_URL}/dashboard`,
+  });
+};
