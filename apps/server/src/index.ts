f4f2047c484e5f0e7f87ebbadc3a3fdb9c0c59da
João Heitor <jhmartinsrdo@gmail.com>
2026-02-09 18:29:43 -0300

    feat: Barber Services Routes

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,17 +2,30 @@ import { auth } from "@barberjs/auth";
 import { env } from "@barberjs/env/server";
 import { cors } from "@elysiajs/cors";
 import { Elysia } from "elysia";
+
 import { CreateBarberController, createBarberBodySchema } from "./controllers/Barber/CreateBarberController";
 import { GetOneBarberController } from "./controllers/Barber/GetOneBarberController";
 import { GetAllBarberController } from "./controllers/Barber/GetAllBarberController";
 import { UpdateBarberController } from "./controllers/Barber/UpdateBarberController";
 import { DeleteBarberController } from "./controllers/Barber/DeleteBarberController";
 
+import { CreateBServiceController, createServiceBodySchema } from "./controllers/BServices/CreateBServiceController";
+import { GetAllBServiceController } from "./controllers/BServices/GetAllBServiceController";
+import { GetOneBServiceController } from "./controllers/BServices/GetOneBServiceController";
+import { UpdateBServiceController } from "./controllers/BServices/UpdateBServiceController";
+import { DeleteBServiceController } from "./controllers/BServices/DeleteBServiceController";
+
 const createBarberController = new CreateBarberController();
 const getOneBarberController = new GetOneBarberController();
 const getAllBarberController = new GetAllBarberController();
 const updateBarberController = new UpdateBarberController();
 const deleteBarberController = new DeleteBarberController();
+
+const createBServiceController = new CreateBServiceController();
+const getAllBServiceController = new GetAllBServiceController();
+const getOneBServiceController = new GetOneBServiceController();
+const updateBServiceController = new UpdateBServiceController();
+const deleteBServiceController = new DeleteBServiceController();
 
 const app = new Elysia()
   .use(
@@ -37,6 +50,13 @@ const app = new Elysia()
   .get("/barber", () => getAllBarberController.handle())
   .put("/barber/:id", (ctx) => updateBarberController.handle(ctx))
   .delete("/barber/:id", (ctx) => deleteBarberController.handle(ctx))
+  .post("/service", (ctx) => createBServiceController.handle(ctx), {
+    body: createServiceBodySchema,
+  })
+  .get("/service", () => getAllBServiceController.handle())
+  .get("/service/:id", (ctx) => getOneBServiceController.handle(ctx))
+  .put("/service/:id", (ctx) => updateBServiceController.handle(ctx))
+  .delete("/service/:id", (ctx) => deleteBServiceController.handle(ctx))
   .get("/", () => "Hello, World!")
   .listen(3333, () => {
     console.log("Server is running on http://localhost:3333");
