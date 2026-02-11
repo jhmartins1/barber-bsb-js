518950e351d08319472a96ea902b85306523ee3a
João Heitor <jhmartinsrdo@gmail.com>
2026-02-11 18:54:09 -0300

    fix prisma env

--- packages/db/prisma.config.ts
+++ packages/db/prisma.config.ts
@@ -1,5 +1,11 @@
+import dotenv from "dotenv";
 import path from "node:path";
 import { defineConfig, env } from "prisma/config";
+
+// tenta carregar .env local se existir
+dotenv.config({
+  path: path.resolve(process.cwd(), "../../apps/server/.env"),
+});
 
 export default defineConfig({
   schema: path.join("prisma", "schema"),
